3ca54c2ddd9dc069057e044b9315eace113f0790
wanghuamin <18219116508@163.com>
2022-06-08 16:27:15 +0800

    配置打包环境变量

--- README.md
+++ README.md
@@ -22,3 +22,33 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+## git 代码管理分支
+```
+framework 项目架构分支
+```
+
+## vue.config.js 项目配置
+```
+配置打包环境变量：
+1.作用：请求的地址在开发线、测试线、和正式线中肯定是不一样的，为了省去每次打包之前去改的麻烦，所以配置打包环境变量解决这个问题。
+
+2.环境变量名字格式为：.env.xxxx，前面.env是固定的后面可以自定义
+local代表本地、development代表开发环境、test代表测试环境、production代表生产环境
+
+3.定义变量
+只有VUE_APP开头的变量和NODE_ENV和BASE_URL才有效哦，所以我定义了如下变量；
+
+4.使用
+① 在package.json里面添加如下代码:
+"scripts": {
+    "dev": "vue-cli-service serve --mode development",
+    "test": "vue-cli-service serve --mode test",
+    "pro": "vue-cli-service serve --mode production",
+    "build": "vue-cli-service build", //默认生产环境
+    "build:test": "vue-cli-service build --mode test"
+  },
+
+② 在项目里调用：process.env.xxxx
+
+```
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,54 @@
 const { defineConfig } = require('@vue/cli-service')
+const { resolve } = require('path')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: '/', // 公共路径
+  indexPath: 'index.html', // 相对于打包路径index.html的路径
+  outputDir: process.env.VUE_APP_OUTPUTDIR || 'dist', // 'dist', 生产环境构建文件的目录
+  assetsDir: 'static', // 相对于outputDir的静态资源(js、css、img、fonts)目录
+  lintOnSave: false, // 是否在开发环境下通过 eslint-loader 在每次保存时 lint 代码
+  runtimeCompiler: true, // 是否使用包含运行时编译器的 Vue 构建版本
+
+  configureWebpack: (config) => {
+    config.resolve = {
+      // 配置解析别名
+      extensions: ['.js', '.json', '.vue'], // 自动添加文件后缀名
+      alias: {
+        '@': resolve(__dirname, './src'),
+        '@assets': resolve(__dirname, './src/assets'),
+        '@components': resolve(__dirname, './src/components')
+      }
+    }
+  },
+
+  devServer: {
+    // 全屏模式下是否显示脚本错误
+    // overlay: {
+    //   // 让浏览器 overlay 同时显示警告和错误
+    //   warnings: true,
+    //   errors: true
+    // },
+    // host: "localhost",
+    port: 9090, // 端口号
+    https: false, // https:{type:Boolean}
+    open: false, // 配置自动启动浏览器
+    // hot: 'only', // 开启热加载
+    // hotOnly: false, // 热更新
+    // 设置代理
+    proxy: {
+      // 配置多个跨域
+      [process.env.VUE_APP_API]: {
+        // 目标代理服务器地址
+        target: process.env.VUE_APP_BASE_URL,
+        // 允许跨域
+        changeOrigin: true,
+        // ws: true,//websocket支持
+        secure: false,
+        pathRewrite: {
+          [`^${process.env.VUE_APP_API}`]: '/'
+        }
+      }
+    }
+  }
 })
